52294906b98c7082de3aef1d1e4908627292a671
Teppei Akada <teppei.akada@sidefeed.com>
2021-05-15 01:51:00 +0900

    とりあえずからのプロジェクトをば

---
+++ SomeSNS/SomeSNS/SomeSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeSNSApp.swift
+//  SomeSNS
+//
+//  Created by AKADA TEPPEI on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SomeSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
